b054eb2411da1d543453c994de718f9dd436d66d
mauroestu <mauroestu18@gmail.com>
2018-03-08 23:24:11 -0600

    se arreglo la funcion para que sea recursiva, factorial usuario1

--- avanzadas.js
+++ avanzadas.js
@@ -1,7 +1,6 @@
-function factorial (n) {
-	var total = 1; 
-	for (i=1; i<=n; i++) {
-		total = total * i; 
+function factorialRecursivo (n) { 
+	if (n == 0){ 
+		return 1; 
 	}
-	return total; 
+	return n * factorialRecursivo (n-1); 
 }
